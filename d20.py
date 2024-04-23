b428edb20b0e59e5349e4644514c11c8ed3e0819
baidan.miha@gmail.com <baidan2009A>
2024-04-23 20:10:56 +0200

    add: d20

---
+++ d20.py
@@ -0,0 +1,29 @@
+import random
+
+skrin = 1
+
+
+def hodk20():
+    return random.randint(1, 20)
+
+
+def main():
+    print("hod kostkou ")
+    print("snažís se posunot skřín to vyžaduje hod na sílu\nzadej k jestli chceš uskutečnit hod")
+    while True:
+        input("zmáčkni enter pro hod")
+        hod = hodk20()
+        print(f"hodil jsi: {hod}+3(tvoje síla) = {hod + 3}")
+        if hod >= 10:
+            print("super uspěšně jsi posunul skřín")
+            global skrin
+            skrin = skrin - 1
+        else:
+            print("mas smulu")
+
+        play_again = input("R: ")
+        if play_again != "y":
+            break
+
+
+main()
